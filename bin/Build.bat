7835742154ce831da1c49437904e7ee149e52576
Josh Fletcher <jfletcher@4d.com>
2012-05-20 23:23:23 -0700

    Forgot to build component for previous check-in. Working on some type of version numbering system.

--- bin/Build.bat
+++ bin/Build.bat
@@ -1,5 +1,6 @@
-set component_dir=..\..\component\CodeExport.4dbase
-set source_dir=.
+set buildNumber=%date:~10,4%%date:~4,2%%date:~7,2%
+set component_dir=..\component\CodeExport.4dbase
+set source_dir=..\matrix\CodeExport.4dbase
 
 rmdir /Q /S %component_dir%
 
@@ -9,7 +10,13 @@ REM Files we need:
 REM   -4DB
 REM   -4DINDY
 REM   -Macros V2
+REM   -Resources
 copy "%source_dir%\CodeExport.4DB" "%component_dir%\CodeExport.4DB"
 copy "%source_dir%\CodeExport.4DIndy" "%component_dir%\CodeExport.4DIndy"
 xcopy "%source_dir%\Macros v2" "%component_dir%\Macros v2" /F /S /I
 xcopy "%source_dir%\Resources" "%component_dir%\Resources" /F /S /I
+
+echo %buildNumber% - Build Complete> build.txt
+type ..\version.txt >> build.txt
+type build.txt > ..\version.txt
+del build.txt
